99b503dedf7e351c4f624730e29c291fabb1bcd2
SamarthBM <samarthbm18@gmail.com>
2021-07-08 09:15:27 +0530

    Added: created a empty tic tac toe board

--- src/com/bridgelabs/TicTacToeGame.java
+++ src/com/bridgelabs/TicTacToeGame.java
@@ -8,8 +8,18 @@ package com.bridgelabs;
 
 public class TicTacToeGame {
 
+	// Method to create a empty tic tac toe board
+	public static void createEmptyBoard() {
+		char[] board = new char[10];
+		for (int a = 0; a < 9; a++) {
+			board[a] = (char) (a+1);
+			System.out.println(" "+ board[a]);
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to tic tac toe game ");
+		createEmptyBoard();
 
 	}
 
